84cbc6d4335335e5ebfe05c25c7cfc7d31bebb88
Caressa Chan <carechan@umich.edu>
2021-04-05 21:08:26 -0500

    stopped in the middle of the tutorial...

--- tomatotimer/app.py
+++ tomatotimer/app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-@app.route('/')
+# Flask-WTF requires an encryption key which can be any string
+app.config['SECRET_KEY'] = 'ySaMndXKbC6xvYP13LJ66ZetQWm3ivlP'
+
+# Flask-Bootstrap requires this line
+Bootstrap(app)
+
+# Create forms here
+class TimeForm(FlaskForm):
+    worktime = IntegerField('Enter work time', validators=[DataRequired()])
+    breaktime = IntegerField('Enter break time', validators=[DataRequired()])
+    submit = SubmitField('Submit preferences')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = TimeForm()
+    message = ''
+    if form.validate_on_submit():
+        worktime = form.worktime.data
+        breaktime = form.breaktime.data
+        # TODO: need to do something here, maybe kick off the timer?
+    else:
+        message = 'Something went wrong. Please try again.'
+    return render_template('index.html', worktime=worktime, breaktime=breaktime, message=message)
